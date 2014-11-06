b5c798f5babaa6f32b53875feeb0770cb2908ce3
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-06 17:42:39 +0000

    Moved statement struct to store.go

--- store.go
+++ store.go
@@ -30,6 +30,19 @@ type InterfaceTable interface {
 	TableName() string
 }
 
+type statement struct {
+	*sqlite3.Stmt
+	vars []string
+}
+
+func (s statement) Vars(t map[string]interface{}) []interface{} {
+	r := make([]interface{}, len(s.vars))
+	for i, v := range s.vars {
+		r[i] = t[v]
+	}
+	return r
+}
+
 type Store struct {
 	db         *sqlite3.Conn
 	mutex      sync.Mutex
