d37e03e5ef1a131713fd718fb30b603ec8fe10ee
Michael Woolnough <michael.woolnough@gmail.com>
2014-11-08 14:37:22 +0000

    Added Tests and corrected problems

--- store.go
+++ store.go
@@ -1,13 +1,12 @@
 package store
 
 import (
-	"fmt"
 	"io"
 	"reflect"
 	"sync"
 	"time"
 
-	"code.google.com/p/go-sqlite/go1/sqlite3"
+	"github.com/mxk/go-sqlite/sqlite3"
 )
 
 const (
@@ -104,6 +103,7 @@ func (s *Store) Register(t Interface) error {
 			vars = append(vars, typeName)
 		}
 	}
+	pVars := append(vars, primary)
 	sql := "CREATE TABLE IF NOT EXISTS [" + tableName + "](" + tableVars + ");"
 	err := s.db.Exec(sql)
 	if err != nil {
@@ -130,7 +130,7 @@ func (s *Store) Register(t Interface) error {
 	if err != nil {
 		return err
 	}
-	s.statements[tableName][update] = statement{Stmt: stmt, vars: append(vars, primary)}
+	s.statements[tableName][update] = statement{Stmt: stmt, vars: pVars}
 
 	sql = "DELETE FROM [" + tableName + "] WHERE [" + primary + "] = ?;"
 	stmt, err = s.db.Prepare(sql)
@@ -139,12 +139,12 @@ func (s *Store) Register(t Interface) error {
 	}
 	s.statements[tableName][remove] = statement{Stmt: stmt}
 
-	sql = "SELECT " + sqlVars + " FROM [" + tableName + "] ORDER BY [" + primary + "] LIMIT ? OFFSET ?;"
+	sql = "SELECT " + sqlVars + ", [" + primary + "] FROM [" + tableName + "] ORDER BY [" + primary + "] LIMIT ? OFFSET ?;"
 	stmt, err = s.db.Prepare(sql)
 	if err != nil {
 		return err
 	}
-	s.statements[tableName][getPage] = statement{Stmt: stmt, vars: vars}
+	s.statements[tableName][getPage] = statement{Stmt: stmt, vars: pVars}
 
 	return nil
 }
@@ -160,7 +160,7 @@ func (s *Store) Set(ts ...Interface) (id int, err error) {
 		if v, ok := p.(*int); ok {
 			primary = *v
 		} else {
-			err = fmt.Errorf("could not decide key")
+			err = WrongKeyType{}
 			return
 		}
 		if primary == 0 {
@@ -194,18 +194,22 @@ func (s *Store) Get(ts ...Interface) error {
 			return err
 		}
 		err = stmt.Scan(stmt.Vars(vars)...)
-		if err != nil {
+		if err == io.EOF {
+			if v, ok := vars[t.Key()].(*int); ok {
+				*v = 0
+			}
+		} else if err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (s *Store) GetPage(data []Interface, offset int) error {
+func (s *Store) GetPage(offset int, data ...Interface) (int, error) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 	if len(data) < 1 {
-		return nil
+		return 0, nil
 	}
 	tableName := TableName(data[0])
 	stmt := s.statements[tableName][getPage]
@@ -214,16 +218,16 @@ func (s *Store) GetPage(data []Interface, offset int) error {
 		pos int
 	)
 	for err = stmt.Query(len(data), offset); err == nil; err = stmt.Next() {
-		err = stmt.Scan(stmt.Vars(data[pos].Get()))
+		err = stmt.Scan(stmt.Vars(data[pos].Get())...)
 		if err != nil {
 			break
 		}
 		pos++
 	}
 	if err != nil && err != io.EOF {
-		return err
+		return pos, err
 	}
-	return nil
+	return pos, nil
 }
 
 func (s *Store) Delete(ts ...Interface) error {
@@ -288,4 +292,12 @@ func unPointer(i interface{}) interface{} {
 	default:
 		return nil
 	}
+}
+
+//Errors
+
+type WrongKeyType struct{}
+
+func (WrongKeyType) Error() string {
+	return "primary key needs to be int"
 }
--- store_test.go
+++ store_test.go
@@ -48,14 +48,17 @@ func (t *testType) Key() string {
 }
 
 func TestSetGet(t *testing.T) {
-	s, err := newTestStore("test")
+	t.Parallel()
+	s, err := newTestStore("testSetGet")
 	defer s.Close()
 	if err != nil {
 		t.Errorf("received unexpected error: %s", err)
+		return
 	}
 	err = s.Register(new(testType))
 	if err != nil {
 		t.Errorf("received unexpected error: %s", err)
+		return
 	}
 	tests := []struct {
 		testType
@@ -92,6 +95,118 @@ func TestSetGet(t *testing.T) {
 			t.Errorf("test 2 %d: received unexpected error: %s", n+1, err)
 		} else if toRet.ID != test.ID || toRet.Data != test.Data || toRet.Number != test.Number {
 			t.Errorf("test 2 %d: returned data does not match expected\nexpecting: %v\ngot: %v", n+1, test.testType, toRet)
+		}
+	}
+}
+
+func TestRemove(t *testing.T) {
+	t.Parallel()
+	s, err := newTestStore("testRemove")
+	defer s.Close()
+	if err != nil {
+		t.Errorf("received unexpected error: %s", err)
+		return
+	}
+	err = s.Register(new(testType))
+	if err != nil {
+		t.Errorf("received unexpected error: %s", err)
+		return
+	}
+	tests := []testType{
+		{0, "HELLO", 12},
+		{0, "WORLD", 14},
+		{0, "Barry", 11},
+	}
+
+	for n, test := range tests {
+		test.ID, err = s.Set(&test)
+		if err != nil {
+			t.Errorf("test %d-1: received unexpected error: %s", n+1, err)
+		}
+		tt := testType{ID: test.ID}
+		err = s.Get(&tt)
+		if err != nil {
+			t.Errorf("test %d-2: received unexpected error: %s", n+1, err)
+		} else if tt.ID != test.ID || tt.Data != test.Data || tt.Number != test.Number {
+			t.Errorf("test %d-2: returned data does not match expected\nexpecting: %v\ngot: %v", n+1, test, tt)
+		}
+		err = s.Delete(&tt)
+		if err != nil {
+			t.Errorf("test %d-3: received unexpected error: %s", n+1, err)
+		}
+		tt = testType{ID: test.ID}
+		err = s.Get(&tt)
+		if err != nil {
+			t.Errorf("test %d-4: received unexpected error: %s", n+1, err)
+		} else if tt.ID != 0 {
+			t.Errorf("test %d-4: expecting id 0, got %d", n+1, tt.ID)
+		}
+	}
+}
+
+func TestGetPage(t *testing.T) {
+	t.Parallel()
+	s, err := newTestStore("testGetPage")
+	defer s.Close()
+	if err != nil {
+		t.Errorf("received unexpected error: %s", err)
+		return
+	}
+	err = s.Register(new(testType))
+	if err != nil {
+		t.Errorf("received unexpected error: %s", err)
+		return
+	}
+	s.Set(
+		&testType{0, "BEEP", 1},
+		&testType{0, "3.14159", 42},
+		&testType{0, "hats", 420},
+		&testType{0, "string", 25},
+	)
+	tests := []struct {
+		toGet, offset int
+		result        []testType
+	}{
+		{1, 0,
+			[]testType{
+				{1, "BEEP", 1},
+			},
+		},
+		{1, 1,
+			[]testType{
+				{2, "3.14159", 42},
+			},
+		},
+		{2, 2,
+			[]testType{
+				{3, "hats", 420},
+				{4, "string", 25},
+			},
+		},
+		{2, 3,
+			[]testType{
+				{4, "string", 25},
+			},
+		},
+	}
+
+	for n, test := range tests {
+		data := make([]testType, test.toGet)
+		idata := make([]Interface, test.toGet)
+		for i := 0; i < test.toGet; i++ {
+			idata[i] = &data[i]
+		}
+		num, err := s.GetPage(test.offset, idata...)
+		if err != nil {
+			t.Errorf("test %d: received unexpected error: %s", n+1, err)
+		} else if num != len(test.result) {
+			t.Errorf("test %d: received %d results, expecting %d", n+1, num, len(test.result))
+		} else {
+			for m, d := range test.result {
+				if d.ID != data[m].ID || d.Data != data[m].Data || d.Number != data[m].Number {
+					t.Errorf("test %d %d: returned data does not match expected\nexpecting: %v\ngot: %v", n+1, m, d, data[m])
+				}
+			}
 		}
 	}
 }
