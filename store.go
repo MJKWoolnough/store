61e24e7ac4cf2edcb47a0fc35fbfe27b88fec6d3
Michael Woolnough <michael.woolnough@gmail.com>
2015-02-19 10:33:51 +0000

    Moved util funcs to utils.go

--- store.go
+++ store.go
@@ -7,7 +7,6 @@ import (
 	"reflect"
 	"strings"
 	"sync"
-	"time"
 )
 
 const (
@@ -227,79 +226,6 @@ func (s *Store) defineType(i interface{}) error {
 		statements: statements,
 	}
 	return nil
-}
-
-func getFieldPointer(i interface{}, fieldNum int) interface{} {
-	v := reflect.ValueOf(i).Elem()
-	if v.NumField() < fieldNum {
-		return nil
-	}
-	if v.Type().Field(fieldNum).PkgPath != "" {
-		return nil
-	}
-	f := v.Field(fieldNum)
-	if f.Kind() == reflect.Ptr {
-		return f.Interface()
-	}
-	return f.Addr().Interface()
-}
-
-func getField(i interface{}, fieldNum int) interface{} {
-	v := reflect.ValueOf(i)
-	if v.NumField() < fieldNum {
-		return nil
-	}
-	if v.Type().Field(fieldNum).PkgPath != "" {
-		return nil
-	}
-	f := v.Field(fieldNum)
-	if f.Kind() == reflect.Ptr {
-		return f.Elem().Interface()
-	}
-	return f.Interface()
-}
-
-func getType(i interface{}, fieldNum int) string {
-	v := getFieldPointer(i, fieldNum)
-	if v == nil {
-		return ""
-	}
-	switch v.(type) {
-	case *int, *int64, *time.Time:
-		return "INTEGER"
-	case *float32, *float64:
-		return "FLOAT"
-	case *string:
-		return "TEXT"
-	case *[]byte:
-		return "BLOB"
-	}
-	return ""
-}
-
-func isValidType(i interface{}) bool {
-	switch i.(type) {
-	case *int, *int64,
-		*string, *float32, *float64, *bool, *time.Time:
-		return true
-	}
-	return false
-}
-
-func isValidKeyType(i interface{}) bool {
-	switch i.(type) {
-	case *int, *int64:
-		return true
-	}
-	return false
-}
-
-func typeName(i interface{}) string {
-	name := reflect.TypeOf(i).String()
-	if name[0] == '*' {
-		name = name[1:]
-	}
-	return name
 }
 
 // Errors
